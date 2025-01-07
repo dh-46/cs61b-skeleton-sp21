592e4cbe48e2c1e4b7440de77e0d8804821b7220
Daniel Huang <danielhuang.work@gmail.com>
2025-01-07 11:48:22 +0800

    Lab 3: Fixing the Bug and Execution Breakpoints

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -2,16 +2,17 @@ package randomizedtest;
 
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+    // YOUR TESTS HERE
 
     @Test
-    public void testThreeAddThreeRemove()  {
+    public void testThreeAddThreeRemove() {
         BuggyAList<Integer> broken = new BuggyAList<>();
         AListNoResizing<Integer> correct = new AListNoResizing<>();
 
@@ -29,6 +30,51 @@ public class TestBuggyAList {
             int removeFromBroken = broken.removeLast();
             int removeFromCorrect = correct.removeLast();
             assertEquals(removeFromCorrect, removeFromBroken);
+        }
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> broken = new BuggyAList<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                correct.addLast(randVal);
+                broken.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int correctSize = correct.size();
+                int brokenSize = broken.size();
+                System.out.println("correct size: " + correctSize);
+                System.out.println("broken size: " + brokenSize);
+                assertEquals(correctSize, brokenSize);
+            } else if (operationNumber == 2) {
+                int size = correct.size();
+
+                if (size > 0) {
+                    int correctLast = correct.getLast();
+                    int brokenLast = broken.getLast();
+
+                    System.out.println("correct getLast: " + correctLast);
+                    System.out.println("broken getLast: " + brokenLast);
+                    assertEquals(correctLast, brokenLast);
+                }
+            } else {
+                if (correct.size() > 0 && broken.size() > 0) {
+                    int correctRemoveLast = correct.removeLast();
+                    int brokenRemoveLast = broken.removeLast();
+                    System.out.println("correctRemoveLast: " + correctRemoveLast);
+                    System.out.println("brokenRemoveLast: " + brokenRemoveLast);
+
+                    assertEquals(correctRemoveLast, brokenRemoveLast);
+                }
+            }
         }
     }
 }
