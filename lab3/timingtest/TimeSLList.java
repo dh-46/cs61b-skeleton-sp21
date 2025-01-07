1b91afd5f22107f1028a06d52d72c2433f83a5b2
Daniel Huang <danielhuang.work@gmail.com>
2025-01-07 10:16:56 +0800

    Lab 3: Timing the getLast method of SLList

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -22,7 +22,60 @@ public class TimeSLList {
     }
 
     public static void timeGetLast() {
-        // TODO: YOUR CODE HERE
+
+        int[] timesArray = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000};
+
+        // 測試項目數
+        AList<Integer> Ns = new AList<>();
+        // 測試花費時間
+        AList<Double> spendTimes = new AList<>();
+        // 方法執行次數
+        AList<Integer> ops = new AList<>();
+
+        int opsCount = 10000;
+
+        for (int N : timesArray) {
+            // Create list
+            SLList<Integer> slList = getTestSLList(N);
+
+            double time = runGetLast(opsCount, slList);
+
+            // 更新測試紀錄
+            spendTimes.addLast(time);
+            ops.addLast(opsCount);
+            Ns.addLast(N);
+        }
+
+        // 印出測試紀錄
+        printTimingTable(Ns, spendTimes, ops);
+    }
+
+    /**
+     * 執行測試
+     * @param opsCount 呼叫次數
+     * @param targetList 測試目標
+     * @return 花費總時間
+     */
+    private static double runGetLast(int opsCount, SLList<Integer> targetList) {
+        // Start timing
+        Stopwatch stopwatch = new Stopwatch();
+
+        // run getLast
+        for (int j = 0; j < opsCount; j++) {
+            targetList.getLast();
+        }
+
+        return stopwatch.elapsedTime();
+    }
+
+    private static SLList<Integer> getTestSLList(int n) {
+        SLList<Integer> slList = new SLList<>();
+
+        for (int i = 0; i < n; i++) {
+            slList.addLast(i);
+        }
+
+        return slList;
     }
 
 }
