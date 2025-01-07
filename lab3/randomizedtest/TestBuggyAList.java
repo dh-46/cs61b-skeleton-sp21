0f766295766d511ea7e1dcfb95e1a0543e2c188a
Daniel Huang <danielhuang.work@gmail.com>
2025-01-07 10:45:34 +0800

    Lab 3: Randomized Comparison Tests-Simple Comparison Test

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,26 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+
+    @Test
+    public void testThreeAddThreeRemove()  {
+        BuggyAList<Integer> broken = new BuggyAList<>();
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+
+        int[] testIntArray = {4, 5, 6};
+
+        for (int i = 0; i < testIntArray.length; i++) {
+            int item = testIntArray[i];
+            broken.addLast(item);
+            correct.addLast(item);
+        }
+
+        assertEquals(correct.size(), broken.size());
+
+        for (int i = 0; i < testIntArray.length; i++) {
+            int removeFromBroken = broken.removeLast();
+            int removeFromCorrect = correct.removeLast();
+            assertEquals(removeFromCorrect, removeFromBroken);
+        }
+    }
 }
