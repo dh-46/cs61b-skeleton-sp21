de678d04ccd8326ff2b89468ce79c9e56f76690f
Daniel Huang <danielhuang.work@gmail.com>
2025-01-14 14:55:34 +0800

    Project 1 EC: TestArrayDequeEC Task 2

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -16,6 +16,7 @@ public class TestArrayDequeEC {
         ArrayDequeSolution<Integer> correct = new ArrayDequeSolution<>();
         StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
 
+        StringBuilder errorMsg = new StringBuilder();
 
         int N = 5000;
         for (int i = 0; i < N; i += 1) {
@@ -26,17 +27,31 @@ public class TestArrayDequeEC {
                 correct.addFirst(randomValue);
                 student.addFirst(randomValue);
 
-                assertEquals(correct.get(0), student.get(0));
+                errorMsg.append("addFirst(" + randomValue + ")\n");
+
+                assertEquals(errorMsg.toString(), correct.get(0), student.get(0));
             } else if (randomOp == 1) {
                 int randomValue = StdRandom.uniform(0, 100);
                 correct.addLast(randomValue);
                 student.addLast(randomValue);
 
-                assertEquals(correct.get(correct.size() - 1), student.get(student.size() - 1));
+                errorMsg.append("addLast(" + randomValue + ")\n");
+
+                assertEquals(errorMsg.toString(), correct.get(correct.size() - 1), student.get(student.size() - 1));
             } else if (randomOp == 2 && !correct.isEmpty() && !student.isEmpty()) {
-                assertEquals(correct.removeFirst(), student.removeFirst());
+                Integer expected = correct.removeFirst();
+                Integer actual = student.removeFirst();
+
+                errorMsg.append("removeFirst()\n");
+
+                assertEquals(errorMsg.toString(), expected, actual);
             } else if (randomOp == 3 && !correct.isEmpty() && !student.isEmpty()) {
-                assertEquals(correct.removeLast(), student.removeLast());
+                Integer expected = correct.removeLast();
+                Integer actual = student.removeLast();
+
+                errorMsg.append("removeLast()\n");
+
+                assertEquals(errorMsg.toString(), expected, actual);
             }
         }
 
