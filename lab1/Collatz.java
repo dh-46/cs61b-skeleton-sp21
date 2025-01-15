1e8248e51208f5e79c8bb3c1f2a93fa872f9b118
Daniel Huang <danielhuang.work@gmail.com>
2025-01-15 09:07:28 +0800

    Lab 1: Done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,10 +2,33 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
+
+    /**
+     * Return next number of n in Collatz sequence
+     * @param n start number
+     */
+    public static int nextNumber(int n) {
+        if (isEven(n)) {
+            // is even
+            return n / 2;
+        } else {
+            // is odd
+            return 3 * n + 1;
+        }
+    }
+
+    private static boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
-
+        while (n != 1) {
+            n = nextNumber(n);
+            System.out.print(n + " ");
+        }
+        System.out.println();
     }
 }
 
