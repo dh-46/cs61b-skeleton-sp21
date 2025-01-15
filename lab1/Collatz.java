2246ab9b8981f8dd53789e7870f8f12725f41171
Daniel Huang <danielhuang.work@gmail.com>
2025-01-15 09:31:02 +0800

    Lab 4B: Checkout Collatz.java to Lab 1 commit

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,15 +3,22 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /**
+     * Return next number of n in Collatz sequence
+     * @param n start number
+     */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (isEven(n)) {
+            // is even
+            return n / 2;
         } else {
-            return n * 2;
+            // is odd
+            return 3 * n + 1;
         }
+    }
+
+    private static boolean isEven(int n) {
+        return n % 2 == 0;
     }
 
     public static void main(String[] args) {
